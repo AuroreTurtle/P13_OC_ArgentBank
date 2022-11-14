bb0d7f003a06a2ee8b9da0f3e71c5c82bd9317af
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-11-14 19:31:57 +0100

    Add page Home

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Header />
+        <Home />
+        <Footer />
     </React.StrictMode>
 );

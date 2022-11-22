d8d70de3f2c0ca22cf7161917119b5f43a69e9aa
Aurore GOURRAUD <aurore.gourraud@gmail.com>
2022-11-22 16:32:33 +0100

    Add folder style

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import "./index.css";
+import "./style/index.css";
 import App from "./pages/App";
 import store from "./app/store";
 
